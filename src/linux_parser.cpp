32c9bf84e15cf1ed2812946c8c6efc8947cf18fb
narek-g <narekengineering@gmail.com>
2022-05-29 12:19:06 -0700

    update code

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -68,7 +68,11 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() { 
+  string memTotal, memFree, memAvailable; 
+  string line, key, value;  
+
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }
