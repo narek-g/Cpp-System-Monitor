21f54638756417ac200b38ae64dd14e9c77de4d8
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-05-31 15:02:39 -0700

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,17 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+    long idle = LinuxParser::IdleJiffies();
+    long nonIdle = LinuxParser::ActiveJiffies();
+    long totalIdle = idle + nonIdle;
+
+    idle_(LinuxParser::IdleJiffies);
+    nonIdle_(LinuxParser::ActiveJiffies)
+    totalIdle_(idle_ + nonIdle_);
+
+    long deltaIdle = idle - idle_;
+    long deltaTotal = totalIdle - totalIdle_;
+
+    return (deltaTotal - deltaIdle)/deltaTotal;
+}
