c673cce357af003890e74fea4faea574bd2584f8
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-05-31 13:29:19 -0700

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,26 @@
 
 using std::string;
 
+
+string Format::NumToString(long number){
+    std::stringstream numstr; 
+
+    if(number < 10){ numstr << "0";}
+    numstr << number; 
+    return numstr.str(); 
+}
+
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    long hour, minute, second; 
+    hour = seconds/3600; 
+    minute = (seconds%3600)/60; 
+    second = minute%60;
+
+    std::stringstream numstr; 
+    numstr << NumToString(hour) << ":" << NumToString(minute) << ":" << NumToString(second);
+    return numstr.str();
+ }
