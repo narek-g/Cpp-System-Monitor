2fbaba7b9e6dd2d9903acd8271d111380c72f8fb
Narek Geghamyan <12162743+narek-g@users.noreply.github.com>
2022-06-02 13:06:09 -0700

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -28,7 +28,7 @@ vector<Process>& System::Processes() {
     for(int pid:pids){
         processes_.push_back(Process(pid));
     }
-    std::sorf(processes_.begin(), processes_.end());
+    std::sort(processes_.begin(), processes_.end());
     return(processes_);
 }
 
